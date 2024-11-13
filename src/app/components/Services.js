54f4d95e1dbd94d51f682d4400e294f8cdb4b99d
samanthkumarsg <samanthkumarsg@gmail.com>
2024-11-13 20:08:01 +0100

    UI components

--- src/app/components/Services.js
+++ src/app/components/Services.js
@@ -7,20 +7,20 @@ export default function Services(){
                 <h1 className="text-lg text-strongText">Our Services</h1>
             </div>
         <div className="w-full sm:w-full md:w-2/4 lg:w-2/4 xl:w-2/4 border flex flex-wrap items-center justify-around gap-4 p-4 mt-4 ">
-            <div className="border-2 border-primary h-40 w-40 rounded-full bg-white flex items-center justify-center">
-                <Image src={'/images/Catering.svg'} className="h-28 w-28" width={100} height={100}  alt="gardo-babo-icons"></Image>
+            <div className="border-2 border-primary  rounded-full bg-white flex items-center justify-center">
+                <Image src={'/images/Catering.svg'} className="h-32 w-32 p-6" width={100} height={100}  alt="gardo-babo-icons"></Image>
             </div>
 
-            <div className="border-2 border-primary h-40 w-40 rounded-full bg-white flex items-center justify-center">
-                <Image src={'/images/Catering.svg'} className="h-28 w-28" width={100} height={100}  alt="gardo-babo-icons"></Image>
+            <div className="border-2 border-primary  rounded-full bg-white flex items-center justify-center">
+                <Image src={'/images/OutsideSeating.svg'} className="h-32 w-32 p-6" width={100} height={100}  alt="gardo-babo-icons"></Image>
             </div>
 
-            <div className="border-2 border-primary h-40 w-40 rounded-full bg-white flex items-center justify-center">
-                <Image src={'/images/Catering.svg'} className="h-28 w-28" width={100} height={100}  alt="gardo-babo-icons"></Image>
+            <div className="border-2 border-primary  rounded-full bg-white flex items-center justify-center">
+                <Image src={'/images/PetsAllowed.svg'} className="h-32 w-32 p-6" width={100} height={100}  alt="gardo-babo-icons"></Image>
             </div>
 
-            <div className="border-2 border-primary h-40 w-40 rounded-full bg-white flex items-center justify-center">
-                <Image src={'/images/Catering.svg'} className="h-28 w-28" width={100} height={100}  alt="gardo-babo-icons"></Image>
+            <div className="border-2 border-primary  rounded-full bg-white flex items-center justify-center">
+                <Image src={'/images/Takeaway.svg'} className="h-32 w-32 p-6" width={100} height={100}  alt="gardo-babo-icons"></Image>
             </div>
         </div>
 
