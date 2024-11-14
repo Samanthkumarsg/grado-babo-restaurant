76d7ce7aeedf54342405ae6a0c198067316a8243
samanth kumar <samanthkumarsg@gmail.com>
2024-11-14 20:55:48 +0100

    Contact component

---
+++ src/app/components/Footer.js
@@ -0,0 +1,12 @@
+export default function Footer() {
+    return (
+        <div className="w-full bg-fill py-20 flex items-center justify-center ">
+            <div className="uppercase text-[#180A0E] flex flex-row gap-x-12 font-medium font-figtree">
+                <h1 className="text-sm">imprint</h1>
+                <h1 className="text-sm">Data Privacy</h1>
+                <h1 className="text-sm">Change cookie settings</h1>
+
+            </div>
+        </div>
+    )
+}
--- src/app/components/Services.js
+++ src/app/components/Services.js
@@ -1,28 +1,33 @@
 import Image from "next/image";
 
-export default function Services(){
-    return(
-        <div className="w-full py-12 flex flex-col items-center justify-around  bg-lightfill">
-            <div className="">  
-                <h1 className="text-lg text-strongText">Our Services</h1>
+export default function Services() {
+    return (
+        <div className="w-full py-28 flex flex-col items-center justify-around  bg-lightfill ">
+            <div className="">
+                <h1 className="text-sm tracking-widest text-strongText uppercase font-medium ">Our Services</h1>
             </div>
-        <div className="w-full sm:w-full md:w-2/4 lg:w-2/4 xl:w-2/4 border flex flex-wrap items-center justify-around gap-4 p-4 mt-4 ">
-            <div className="border-2 border-primary  rounded-full bg-white flex items-center justify-center">
-                <Image src={'/images/Catering.svg'} className="h-32 w-32 p-6" width={100} height={100}  alt="gardo-babo-icons"></Image>
-            </div>
+            <div className="w-full sm:w-full md:w-3/4 lg:w-3/4 xl:w-3/4 border flex flex-wrap items-center justify-around gap-4 p-4 mt-8 ">
 
-            <div className="border-2 border-primary  rounded-full bg-white flex items-center justify-center">
-                <Image src={'/images/OutsideSeating.svg'} className="h-32 w-32 p-6" width={100} height={100}  alt="gardo-babo-icons"></Image>
-            </div>
+                <div className=" shadow-lg shadow-fill  rounded-full  bg-white flex items-center justify-center flex-col w-48 h-48">
+                    <Image src={'/images/Catering.svg'} className="h-24 w-24 " width={100} height={100} alt="gardo-babo-icons"></Image>
+                    <h1 className="text-base text-strongText mt-4  ">Catering</h1>
+                </div>
 
-            <div className="border-2 border-primary  rounded-full bg-white flex items-center justify-center">
-                <Image src={'/images/PetsAllowed.svg'} className="h-32 w-32 p-6" width={100} height={100}  alt="gardo-babo-icons"></Image>
-            </div>
+                <div className="border shadow-lg shadow-fill  rounded-full bg-white flex items-center justify-center flex-col w-48 h-48">
+                    <Image src={'/images/OutsideSeating.svg'} className="h-24 w-24 " width={100} height={100} alt="gardo-babo-icons"></Image>
+                    <h1 className="text-base text-strongText mt-4 ">Our Seating</h1>
+                </div>
 
-            <div className="border-2 border-primary  rounded-full bg-white flex items-center justify-center">
-                <Image src={'/images/Takeaway.svg'} className="h-32 w-32 p-6" width={100} height={100}  alt="gardo-babo-icons"></Image>
+                <div className="border shadow-lg shadow-fill  rounded-full bg-white flex items-center justify-center flex-col w-48 h-48">
+                    <Image src={'/images/PetsAllowed.svg'} className="h-24 w-24 " width={100} height={100} alt="gardo-babo-icons"></Image>
+                    <h1 className="text-base text-strongText mt-4 ">Takeaway</h1>
+                </div>
+
+                <div className="border shadow-lg shadow-fill  rounded-full  bg-white flex items-center justify-center flex-col w-48 h-48">
+                    <Image src={'/images/Takeaway.svg'} className="h-24 w-24" width={100} height={100} alt="gardo-babo-icons"></Image>
+                    <h1 className="text-base text-strongText mt-4 ">Pets Allowed</h1>
+                </div>
             </div>
-        </div>
 
         </div>
     )
