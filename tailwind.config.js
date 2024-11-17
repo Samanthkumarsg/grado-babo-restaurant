f079c4a37a37cb7077f7292144b7ec182555d99d
samanth kumar <samanthkumarsg@gmail.com>
2024-11-17 05:01:51 +0100

    Implemented Responsive Design

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,22 +10,21 @@ module.exports = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
-        primary:"#D03970",
+        primary: "#D03970",
         fill: "#F7DDE3",
-        lightfill:"#FFF2F5",
+        lightfill: "#FFF2F5",
         weak: "#D5D1D2",
         strong: "#330C1A",
         textWeak: "#765B64",
-        strongText: "#330C1A"
+        strongText: "#330C1A",
       },
       fontFamily: {
         playfair: ['Playfair Display', 'serif'],
         figtree: ['Figtree', 'sans-serif'],
       },
-      backgroundImage:{
+      backgroundImage: {
         hero: "url('/images/background.svg')",
         heroRight: "url('/images/heroImage.png')",
-
       }
     },
   },
